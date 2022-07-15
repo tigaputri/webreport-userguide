789c364ecc9200d5eb2dc0d5a88176e817b75cfc
TigaPutri VS <vs.tigaputri@outlook.com>
2022-07-16 01:44:15 +0700

    Change github projectName at docusaurus.config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -18,7 +18,7 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'tigaputri', // Usually your GitHub org/user name.
-  projectName: 'wr-userguide', // Usually your repo name.
+  projectName: 'webreport-userguide', // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
